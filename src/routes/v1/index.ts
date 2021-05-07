6151ca9c4debef6648904177833dfe538dd7b812
konarkkapil1 <konarkkapil@rocketmail.com>
2021-05-08 00:12:26 +0530

    added auth middleware for protected routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import Signup from './auth/signup';
+import Authentication from '../../middlwares/Authentication';
 import { NotFound } from '../../response/Response';
 
 export default class Routes {
@@ -17,6 +18,13 @@ export default class Routes {
             @@PATH = /auth/signup
         */
         this.app.use('/auth', new Signup(this.app).getRoute());
+
+        //Authentication middleware
+        this.app.use(new Authentication(this.app).authenticate());
+
+        this.app.get('/index', (req,res) => {
+            res.json({message: "success", user: (<any>req).user});
+        })
         
         //handles 404 routes
         this.app.use((req,res,next) => {
